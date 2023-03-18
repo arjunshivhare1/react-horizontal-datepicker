6307a9dfdcd18541b0d114eb25362fec7bb9272d
arjun shivhare <arjunshivhare22@gmail.com>
2023-03-18 22:01:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { DatePicker } from "./components/DatePicker";
 
 function App() {
-  const selectedDay = (val) =>{
-      console.log(val)
+  const selectedDay = (val) => {
+    console.log(val);
   };
 
   const startDate = new Date(2010, 0, 1);
 
   return (
     <div className="App">
-        <DatePicker startDate={startDate} 
-                    days={366 * 25}
-                    type="month"
-                    selectDate={new Date(2021, 9, 1)}
-                    getSelectedDay={selectedDay} 
-                    labelFormat={"MMMM yyyy"} 
-                    color={"#374e8c"}/>
+      <DatePicker
+        startDate={startDate}
+        days={366 * 25}
+        selectDate={new Date()}
+        getSelectedDay={selectedDay}
+        labelFormat={"MMMM yyyy"}
+        color={"#374e8c"}
+      />
     </div>
   );
 }
